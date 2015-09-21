0beafacb960139eae3c768d0dff37f1b9f402955
Jako <Jako8809@gmail.com>
2015-09-21 11:11:47 +0300

    Execute service.

--- app/src/main/java/com/wc00/MainActivity.java
+++ app/src/main/java/com/wc00/MainActivity.java
@@ -1,17 +1,30 @@
 package com.wc00;
 
 import android.app.Activity;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.AttributeSet;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+
+import com.wc00.media.PlayerService;
 
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements View.OnClickListener {
+
+    private Button playButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        playButton = (Button) findViewById(R.id.PlayButton);
+        playButton.setOnClickListener(this);
     }
 
     @Override
@@ -34,5 +47,15 @@ public class MainActivity extends Activity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v == playButton) {
+            Intent intent = new Intent(this, PlayerService.class);
+            intent.setAction(PlayerService.ACTION_PLAY);
+            intent.putExtra(PlayerService.EXTRA_FILE_PATH, "filePath");
+            startService(intent);
+        }
     }
 }
